d22b597ffade18e546a8590a957ddfbb6d667f0e
Adem-Tozlu <Adem-Tozlu@hotmail.de>
2024-09-05 16:31:25 +0200

    Project Social Network begonnen

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import '../src/styles/scss/main.scss'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
